381c87317c3eb5cc373a3c1009905fca1c6356de
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-25 15:18:39 -0300

    [Desafio 2] correçao2

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,16 @@
-SELECT JOB_TITLE AS `Cargo`, CASE WHEN 5000 < MAX_SALARY AND MAX_SALARY > 10000 THEN "Baixo" WHEN 10001 < MAX_SALARY AND MAX_SALARY > 20000 THEN "Médio" WHEN 20001 < MAX_SALARY AND MAX_SALARY > 30000 THEN "Alto" ELSE "Altíssimo" END AS `Nível` FROM hr.jobs ORDER BY `Cargo`;
+SELECT
+    JOB_TITLE AS `Cargo`,
+    CASE
+        WHEN 5000 < MAX_SALARY
+        AND MAX_SALARY < 10000 THEN "Baixo"
+        WHEN 10001 < MAX_SALARY
+        AND MAX_SALARY < 20000 THEN "Médio"
+        WHEN 20001 < MAX_SALARY
+        AND MAX_SALARY < 30000 THEN "Alto"
+        ELSE "Altíssimo"
+    END AS `Nível`
+FROM
+    hr.jobs
+ORDER BY
+    `Cargo`;
+    
