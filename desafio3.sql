4e67f787194951dd2eae27b38f0245de6e5e267d
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-25 15:33:00 -0300

    [Desafio 3] resoluçao

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+SELECT
+    JOB_TITLE AS "Cargo",
+    MAX_SALARY - MIN_SALARY AS "Diferença entre salários máximo e mínimo"
+FROM
+    hr.jobs
+ORDER BY
+    `Diferença entre salários máximo e mínimo`,
+    `Cargo`;
