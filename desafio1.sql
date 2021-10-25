d8385674592844b6f883f5a76ef90c948e64f8cd
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-25 14:35:43 -0300

    teste style

--- desafio1.sql
+++ desafio1.sql
@@ -1,11 +1,1 @@
-SELECT
-    COUNTRY_NAME AS PAÍS,
-    IF(
-        REGION_ID = 1,
-        'incluído',
-        'não incluído'
-    ) AS 'STATUS INCLUSÃO'
-FROM
-    hr.countries
-ORDER BY
-    PAÍS;
+SELECT COUNTRY_NAME AS PAÍS, IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'STATUS INCLUSÃO' FROM hr.countries ORDER BY PAÍS;
