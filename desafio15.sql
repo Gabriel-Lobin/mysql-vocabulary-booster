c39290c030a0e2236bc2ee0a559ff71cf61ee879
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-25 19:37:06 -0300

    [Desafio 15] resoluçao

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+DELIMITER $$ 
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(25), OUT media INT) 
+BEGIN
+    SELECT
+        ROUND(AVG(E.SALARY), 2) AS `Média salarial`
+    FROM
+        hr.jobs AS J
+        INNER JOIN 
+        hr.employees AS E 
+        ON E.JOB_ID = J.JOB_ID
+    WHERE
+        cargo = J.JOB_TITLE;
+
+END $$ 
+DELIMITER ;
