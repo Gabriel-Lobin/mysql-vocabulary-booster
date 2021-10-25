880d0f31231437fcaa2f95506e5ed22dd5da1d80
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-25 14:48:26 -0300

    [Desafio 1] style2

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,4 @@
-SELECT CO.COUNTRY_NAME AS PAÍS, IF(RE.REGION_NAME = "Europe", 'incluído', 'não incluído') AS 'STATUS INCLUSÃO' FROM hr.countries AS CO LEFT JOIN hr.regions AS RE ON CO.REGION_ID = RE.REGION_ID
+SELECT CO.COUNTRY_NAME AS PAÍS, IF(RE.REGION_NAME = "Europe", 'incluído', 'não incluído') AS 'STATUS INCLUSÃO' FROM hr.countries AS CO 
+LEFT JOIN hr.regions AS RE 
+ON CO.REGION_ID = RE.REGION_ID
 ORDER BY PAÍS;
