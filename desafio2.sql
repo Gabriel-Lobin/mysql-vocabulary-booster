b4cdab1b1148fd9369b5977fe76d0538587b1c40
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-25 15:25:41 -0300

    [Desafio 2] resoluçao

--- desafio2.sql
+++ desafio2.sql
@@ -1,12 +1,12 @@
 SELECT
     JOB_TITLE AS `Cargo`,
     CASE
-        WHEN 5000 < MAX_SALARY
-        AND MAX_SALARY < 10000 THEN "Baixo"
-        WHEN 10001 < MAX_SALARY
-        AND MAX_SALARY < 20000 THEN "Médio"
-        WHEN 20001 < MAX_SALARY
-        AND MAX_SALARY < 30000 THEN "Alto"
+        WHEN 5000 <= MAX_SALARY
+        AND MAX_SALARY <= 10000 THEN "Baixo"
+        WHEN 10001 <= MAX_SALARY
+        AND MAX_SALARY <= 20000 THEN "Médio"
+        WHEN 20001 <= MAX_SALARY
+        AND MAX_SALARY <= 30000 THEN "Alto"
         ELSE "Altíssimo"
     END AS `Nível`
 FROM
